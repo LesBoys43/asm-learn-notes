0685f8382587fa8d701a61c24dec6a25c892a31b
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 06:06:47 +0000

    prepare multi testcases, impl testcase.php for other important pages

--- pages/Prog:数组求和/testcases.php
+++ pages/Prog:数组求和/testcases.php
@@ -1,2 +1,5 @@
 <?php
-$testcases = [];
+$testcases = [
+	["excepted_rbx_val" => "44", "content_S_sed_pattern" => ["1, 2, 3", "22, 21, 1"]],
+	["excepted_rbx_val" => "1000", "content_S_sed_pattern" => ["1, 2, 3", "100, 200, 300, 400"]]
+];
--- pages/Prog:游程统计/testcases.php
+++ pages/Prog:游程统计/testcases.php
@@ -1,2 +1,5 @@
 <?php
-$testcases = [];
+$testcases = [
+	["excepted_rbx_val" => "8", "content_S_sed_pattern" => ["13915", "1435"]],
+	["excepted_rbx_val" => "9", "content_S_sed_pattern" => ["13915", "1442"]]
+];
