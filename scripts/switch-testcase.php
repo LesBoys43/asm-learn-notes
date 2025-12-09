5b404b2dd690d95a3a20fe838b3063898c8da9c7
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 06:01:12 +0000

    add script for switch testcase

---
+++ scripts/switch-testcase.php
@@ -0,0 +1,10 @@
+<?php
+$prog = $argv[1];
+$tc = intval($argv[2]);
+chdir("pages/Prog:$prog");
+include 'testcases.php';
+if (isset($testcases[$tc])) {
+	file_put_contents('excepted_rbx_val.txt', $testcases[$tc]['excepted_rbx_val']);
+	system("sed content.S -i -e s/${$testcases[$tc]['content_S_sed_pattern'][0]}/${$testcases[$tc]['content_S_sed_pattern'][1]}/");
+}
+chdir("../../");
