b825cc8a0aa0752a102ac05d4563168b388e64ec
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 06:46:09 +0000

    remove space to ensure sed works

--- pages/Prog:数组求和/testcases.php
+++ pages/Prog:数组求和/testcases.php
@@ -1,5 +1,5 @@
 <?php
 $testcases = [
-	["excepted_rbx_val" => "44", "content_S_sed_pattern" => ["1, 2, 3", "22, 21, 1"]],
-	["excepted_rbx_val" => "1000", "content_S_sed_pattern" => ["1, 2, 3", "100, 200, 300, 400"]]
+	["excepted_rbx_val" => "44", "content_S_sed_pattern" => ["1,2,3", "22,21,1"]],
+	["excepted_rbx_val" => "1000", "content_S_sed_pattern" => ["1,2,3", "100,200,300,400"]]
 ];
