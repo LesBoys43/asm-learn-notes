cfc4caa94d48321c4f73425c7ae3581515e28887
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 05:51:33 +0000

    prepare multi testcases, add more testcases for page 阶乘

---
+++ pages/Prog:阶乘/testcases.php
@@ -0,0 +1,5 @@
+<?php
+$testcases = [
+  ["excepted_rbx_val" => 362880, "content_S_sed_pattern" => ["14", "9"]],
+  ["excepted_rbx_val" => 2432902008176640000, "content_S_sed_pattern" => ["14", "20"]]  
+];
