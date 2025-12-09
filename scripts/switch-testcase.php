333659da7260e99325d40cc369bd37476bac4b20
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 06:13:48 +0000

    script fixes (another)

--- scripts/switch-testcase.php
+++ scripts/switch-testcase.php
@@ -5,8 +5,8 @@ chdir("pages/Prog:$prog");
 include 'testcases.php';
 if (isset($testcases[$tc])) {
 	file_put_contents('excepted_rbx_val.txt', $testcases[$tc]['excepted_rbx_val']);
-	$src = testcases[$tc]['content_S_sed_pattern'][0];
-	$dest = testcases[$tc]['content_S_sed_pattern'][1];
+	$src = $testcases[$tc]['content_S_sed_pattern'][0];
+	$dest = $testcases[$tc]['content_S_sed_pattern'][1];
 	system("sed content.S -i -e 's/$src/$dest/'");
 }
 chdir("../../");
