5ef58c12110dd35b2e9f3103ca53bcc59ca5a0fc
LBLaiSiNanHai <lesboyspp43@outlook.com>
2025-12-09 06:25:47 +0000

    make testcase switcher as function

--- scripts/switch-testcase.php
+++ scripts/switch-testcase.php
@@ -1,12 +1,12 @@
 <?php
-$prog = $argv[1];
-$tc = intval($argv[2]) - 2;
-chdir("pages/Prog:$prog");
-include 'testcases.php';
-if (isset($testcases[$tc])) {
-	file_put_contents('excepted_rbx_val.txt', $testcases[$tc]['excepted_rbx_val']);
-	$src = $testcases[$tc]['content_S_sed_pattern'][0];
-	$dest = $testcases[$tc]['content_S_sed_pattern'][1];
-	system("sed content.S -i -e 's/$src/$dest/'");
+function switch_testcase ($prog, $tc) {
+	chdir("pages/Prog:$prog");
+	include 'testcases.php';
+	if (isset($testcases[$tc])) {
+		file_put_contents('excepted_rbx_val.txt', $testcases[$tc]['excepted_rbx_val']);
+		$src = $testcases[$tc]['content_S_sed_pattern'][0];
+		$dest = $testcases[$tc]['content_S_sed_pattern'][1];
+		system("sed content.S -i -e 's/$src/$dest/'");
+	}
+	chdir("../../");
 }
-chdir("../../");
